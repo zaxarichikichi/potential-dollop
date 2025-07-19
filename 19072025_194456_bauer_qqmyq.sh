76b0a44812ad0f4f14aa7c804d61d847faec03af
zaxarichikichi <levanmarghia23@gmail.com>
2025-07-19 19:44:56 +0200

    Setting bauer

---
+++ 19072025_194456_bauer_qqmyq.sh
@@ -0,0 +1,1 @@
+During World War II, there was not enough sugar in the U.S. to make candy as it was sent to the troops overseas. At this time, popcorn was consumed three times more than its usual amount
